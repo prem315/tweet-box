69995339448aeff7b2c01a3a79c3da7978b72b0b
Prem Patel <prem.patel3105@gmail.com>
2017-07-16 10:17:54 +0530

    designed tweet box

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react';
 //import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: "",
+    }
+  }
   render() {
     return (
-      <div className="btn btn-primary">Tweet</div>
+      <div className="well clearfix">
+        <h1>Tweet Box Example</h1>
+        <textarea className="form-control"></textarea><br/>
+        <button className="btn btn-primary pull-right"
+          disabled
+          >Tweet</button>
+      </div>
     );
   }
 }
